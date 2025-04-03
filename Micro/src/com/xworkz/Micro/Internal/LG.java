b86fe99ed73b64cc1269081fb1c0e3a3f64de320
SnehaTalekar26 <talekarsneha26@gmail.com>
2025-04-03 08:36:35 +0530

    Methods

---
+++ Micro/src/com/xworkz/Micro/Internal/LG.java
@@ -0,0 +1,7 @@
+package com.xworkz.Micro.Internal;
+
+public class LG extends TV{
+    public LG(){
+        System.out.println("No args const in LG");
+    }
+}
---
+++ Micro/src/com/xworkz/Micro/Internal/Milton.java
@@ -0,0 +1,7 @@
+package com.xworkz.Micro.Internal;
+
+public class Milton extends Bottle{
+    public Milton(){
+        System.out.println("No args const in Milton");
+    }
+}
