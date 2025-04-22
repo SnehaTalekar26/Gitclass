96accd772a7b0962641c530292dfa04b750b6dbc
SnehaTalekar26 <talekarsneha26@gmail.com>
2025-04-23 02:05:27 +0530

    Interface

---
+++ Latent/src/com/xworkz/Latent/internal/IndianCricketTeam.java
@@ -0,0 +1,11 @@
+package com.xworkz.Latent.internal;
+
+public class IndianCricketTeam implements Icc{
+    public IndianCricketTeam(){
+        System.out.println("Running IndianCricketTeam const");
+    }
+    @Override
+    public void fairPlay(){
+        System.out.println("IndianCricketTeam is running in Icc");
+    }
+}
